7bf163f2b4fe0d18b617fb9264f4e72c37a39dc5
asif.islam <asif07c@gmail.com>
2021-10-27 01:11:43 +0600

    53. Maximum Subarray (Kadanes Algorithm) solution in python

---
+++ 53_Maximum_Subarray.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxSubArray(self, nums: list[int]) -> int:
+        max_local = max_global = nums[0]
+
+        for i in range(1, len(nums)):
+            max_local = max(nums[i], max_local + nums[i])
+            if max_local > max_global:
+                max_global = max_local
+
+        return max_global
+
+
+print(Solution().maxSubArray([1, 2, -5, 1, -3, 2]))
