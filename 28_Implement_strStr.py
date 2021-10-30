28669568b786a00d59c7bba441956465c04bc032
asif.islam <asif07c@gmail.com>
2021-10-30 14:56:58 +0600

    indentations changed

--- 28_Implement_strStr.py
+++ 28_Implement_strStr.py
@@ -2,9 +2,9 @@ class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         if needle == haystack or needle == "":
             return 0
-        else: 
+        else:
             for i in range(len(haystack)):
-                if(haystack[i:i+len(needle)] == needle):
+                if haystack[i : i + len(needle)] == needle:
                     return i
-                
+
             return -1
