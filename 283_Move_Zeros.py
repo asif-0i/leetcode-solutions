48b47b23212ae9ce37d8226bbeb9709a8592ddee
asif.islam <asif07c@gmail.com>
2021-10-31 13:44:03 +0600

    283. Move Zeros in python

---
+++ 283_Move_Zeros.py
@@ -0,0 +1,23 @@
+class Solution:
+    """
+    def moveZeroes(self, nums: list[int]) -> None:
+        nonZeroTimes = 0
+
+        for n in nums:
+            if n != 0:
+                nums[nonZeroTimes] = n
+                nonZeroTimes += 1
+
+        for i in range(nonZeroTimes, len(nums)):
+            nums[i] = 0
+    """
+
+    def moveZeroes(self, nums):
+        pos = 0
+
+        for idx, n in enumerate(nums):
+            if n != 0:
+                nums[pos], nums[idx] = n, nums[pos]
+                pos += 1
+
+                # print(nums)
