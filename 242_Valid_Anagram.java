7cc8f6c041e52fea0d8ccdfb6cbeaed736a05d39
asif.islam <asif07c@gmail.com>
2021-12-07 00:34:06 +0600

    242. Valid Anagram solution added in java

---
+++ 242_Valid_Anagram.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        
+        int[] freq = new int[26];
+        
+        for (int i=0; i<s.length(); i++)
+            freq[s.charAt(i) - 'a'] += 1;
+        
+        for(int i=0; i<t.length(); i++)
+        {
+            if (freq[t.charAt(i) - 'a'] == 0) return false;
+            
+            freq[t.charAt(i) - 'a'] -= 1;
+        }
+        
+        return true;
+    }
+}
